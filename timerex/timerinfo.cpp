20bd574f29bbba9bf66ad2220a6827b4a51f3537
CirnoV <cirnov59@gmail.com>
2021-06-15 17:59:27 +0900

    초기화되지 않은 변수 사용 금지

--- timerex/timerinfo.cpp
+++ timerex/timerinfo.cpp
@@ -2,24 +2,26 @@
 #include "extension.h"
 
 TimerInfo::TimerInfo(IPluginFunction *hook, IPluginContext *context, int interval, int userData, int flags)
-  : mHook(hook), mContext(context), mInterval(interval), mUserData(userData), mFlags(flags)
+    : mHook(hook), mContext(context), mInterval(interval), mUserData(userData), mFlags(flags)
 {
   auto start = std::chrono::system_clock::now();
   auto time = start + std::chrono::milliseconds(interval);
   mTime = time;
 }
 
-TimerInfo::~TimerInfo() {
-  if (mFlags & TIMER_DATA_HNDL_CLOSE) {
+TimerInfo::~TimerInfo()
+{
+  if (mFlags & TIMER_DATA_HNDL_CLOSE)
+  {
     HandleSecurity sec;
-    HandleError herr;
     Handle_t usrhndl = static_cast<Handle_t>(mUserData);
 
     sec.pOwner = mContext->GetIdentity();
     sec.pIdentity = g_pCoreToken;
 
-    herr = handlesys->FreeHandle(usrhndl, &sec);
-    if (herr != HandleError_None) {
+    HandleError herr = handlesys->FreeHandle(usrhndl, &sec);
+    if (herr != HandleError_None)
+    {
       smutils->LogError(myself, "Invalid data handle %x (error %d) passed during timer end with TIMER_DATA_HNDL_CLOSE", usrhndl, herr);
     }
   }
