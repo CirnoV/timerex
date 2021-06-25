d43ffc7ceab7e67a93bb0a8db2b03b5df7a779e6
CirnoV <cirnov59@gmail.com>
2021-06-26 01:39:41 +0900

    Github Actions 적용 / AMBuild 2.2

--- configure.py
+++ configure.py
@@ -1,20 +1,29 @@
-# vim: set sts=2 ts=8 sw=2 tw=99 et:
+# vim: set sts=4 ts=8 sw=4 tw=99 et:
 import sys
-from ambuild2 import run
+API_VERSION = '2.2'
 
-# Simple extensions do not need to modify this file.
+try:
+    from ambuild2 import run
+    if not run.HasAPI(API_VERSION):
+        raise Exception()
+except:
+    sys.stderr.write(
+        'AMBuild {0} must be installed to build this project.\n'.format(API_VERSION))
+    sys.stderr.write('http://www.alliedmods.net/ambuild\n')
+    sys.exit(1)
 
-builder = run.PrepareBuild(sourcePath=sys.path[0])
-
-builder.options.add_option('--core-path', type=str, dest='core_path', default=None,
-                           help='Core library path')
-builder.options.add_option('--mms-path', type=str, dest='mms_path', default=None,
-                           help='Path to Metamod:Source')
-builder.options.add_option('--sm-path', type=str, dest='sm_path', default=None,
-                           help='Path to SourceMod')
-builder.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
-                           help='Enable debugging symbols')
-builder.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
-                           help='Enable optimization')
+builder = run.BuildParser(sourcePath=sys.path[0], api=API_VERSION)
+builder.options.add_argument('--core-path', type=str, dest='core_path', default=None,
+                             help='Core library path')
+builder.options.add_argument('--mms-path', type=str, dest='mms_path', default=None,
+                             help='Path to Metamod:Source')
+builder.options.add_argument('--sm-path', type=str, dest='sm_path', default=None,
+                             help='Path to SourceMod')
+builder.options.add_argument('--enable-debug', action='store_const', const='1', dest='debug',
+                             help='Enable debugging symbols')
+builder.options.add_argument('--enable-optimize', action='store_const', const='1', dest='opt',
+                             help='Enable optimization')
+builder.options.add_argument('--targets', type=str, dest='targets', default=None,
+		                      help="Override the target architecture (use commas to separate multiple targets).")
 
 builder.Configure()
